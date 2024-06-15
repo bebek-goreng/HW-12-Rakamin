9965d4ba94cf1fffd3d874edf19d44c13de87ffe
Ian <fyanmahendra@gmail.com>
2024-06-15 19:45:44 +0700

    Complete hw rakamin

--- src/App.js
+++ src/App.js
@@ -1,55 +1,166 @@
-
 import * as React from 'react';
+import {
+  ChakraProvider,
+  Button,
+  Flex,
+  Text,
+  Switch,
+  useColorMode,
+  ColorModeProvider,
+} from '@chakra-ui/react';
 
 function Board() {
-  const squares = Array(9).fill(null);
-  function selectSquare(square) {
+  const [history, setHistory] = React.useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = React.useState(0);
+  const [nextValue, setNextValue] = React.useState('X');
 
+  function selectSquare(square) {
+    if (calculateWinner(history[stepNumber]) || history[stepNumber][square]) {
+      return;
+    }
+    const newHistory = history.slice(0, stepNumber + 1);
+    const currentSquares = newHistory[newHistory.length - 1];
+    const newSquares = [...currentSquares];
+    newSquares[square] = nextValue;
+    setHistory([...newHistory, newSquares]);
+    setStepNumber(newHistory.length);
+    setNextValue(calculateNextValue(newSquares));
   }
 
   function restart() {
+    setHistory([Array(9).fill(null)]);
+    setStepNumber(0);
+    setNextValue('X');
+  }
+
+  function undo() {
+    if (stepNumber > 0) {
+      setStepNumber(stepNumber - 1);
+      setNextValue(calculateNextValue(history[stepNumber - 1]));
+    }
+  }
+
+  function redo() {
+    if (stepNumber < history.length - 1) {
+      setStepNumber(stepNumber + 1);
+      setNextValue(calculateNextValue(history[stepNumber + 1]));
+    }
   }
 
   function renderSquare(i) {
     return (
-      <button className="square" onClick={() => selectSquare(i)}>
-        {squares[i]}
-      </button>
+      <Button
+        className="square"
+        variant="outline"
+        size="lg"
+        fontSize="2xl"
+        fontWeight="bold"
+        width="100px"
+        height="100px"
+        onClick={() => selectSquare(i)}
+      >
+        {history[stepNumber][i]}
+      </Button>
     );
   }
 
+  function Title() {
+    return (
+      <div>
+        <h2>Tic Tac Toe</h2>
+      </div>
+    )
+  }
+
+  const currentSquares = history[stepNumber];
+  const winner = calculateWinner(currentSquares);
+  const status = calculateStatus(winner, currentSquares, nextValue);
+
   return (
-    <div>
-      <div >STATUS</div>
-      <div >
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div >
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div >
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-      <button onClick={restart}>
-        restart
-      </button>
-    </div>
+    <Flex direction="column" alignItems="center" justifyContent="center">
+      <Text fontSize="3xl" fontWeight="bold" mb="4" textAlign="center">
+        <Title />
+      </Text>
+      <Text fontSize="xl" fontWeight="bold" mb="4" textAlign="center">
+        {status}
+      </Text>
+      <Flex className="board" direction="column" alignItems="center" justifyContent="center">
+        <Flex className="board-row" mb="2">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </Flex>
+        <Flex className="board-row" mb="2">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </Flex>
+        <Flex className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </Flex>
+      </Flex>
+      <Flex direction="row" alignItems="center" justifyContent="center" mt="4">
+        <Button
+          onClick={undo}
+          colorScheme="blue"
+          variant="solid"
+          size="md"
+          disabled={stepNumber === 0}
+          mr="2"
+          opacity={stepNumber === 0 ? '0.5' : '1'}
+          width="100px"
+        >
+          Undo
+        </Button>
+        <Button
+          onClick={redo}
+          colorScheme="blue"
+          variant="solid"
+          size="md"
+          disabled={stepNumber === history.length - 1 || winner}
+          mr="2"
+          opacity={stepNumber === history.length - 1 || winner ? '0.5' : '1'}
+          width="100px"
+        >
+          Redo
+        </Button>
+        <Button
+          onClick={restart}
+          colorScheme="blue"
+          variant="solid"
+          size="md"
+          width="100px"
+        >
+          Restart
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
 
 function Game() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <div >
-      <div >
-        <Board />
-      </div>
-    </div>
+    <Flex direction="column" alignItems="center" justifyContent="center" minHeight="100vh">
+      <Flex
+        position="absolute"
+        top="2"
+        right="2"
+        mr="4"
+        mt="4"
+      >
+        <Switch
+          colorScheme="blue"
+          isChecked={colorMode === 'dark'}
+          onChange={toggleColorMode}
+          size="md"
+        />
+      </Flex>
+      <Board />
+    </Flex>
   );
 }
 
@@ -58,8 +169,8 @@ function calculateStatus(winner, squares, nextValue) {
   return winner
     ? `Winner: ${winner}`
     : squares.every(Boolean)
-      ? `Scratch: Cat's game`
-      : `Next player: ${nextValue}`;
+    ? `Scratch: Cat's game`
+    : `${nextValue} Turn`;
 }
 
 // eslint-disable-next-line no-unused-vars
@@ -89,7 +200,13 @@ function calculateWinner(squares) {
 }
 
 function App() {
-  return <Game />;
+  return (
+    <ChakraProvider>
+      <ColorModeProvider>
+        <Game />
+      </ColorModeProvider>
+    </ChakraProvider>
+  );
 }
 
 export default App;
